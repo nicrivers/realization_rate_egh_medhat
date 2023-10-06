622d025c938dbdab3a3f80cecff26446431210d3
nicrivers <ncrivers@uottawa.ca>
2023-10-06 14:03:10 -0400

    updating distributional analysis with estimates of savings

--- scripts/09_distributional_graphs.R
+++ scripts/09_distributional_graphs.R
@@ -108,3 +108,75 @@ ggplot(hd,
   scale_x_continuous(name="Average assessed value", labels=scales::dollar_format()) +
   theme_bw()
 ggsave("../output_figures_tables/count_vs_assessvalue_all.png", width=6, height=6)
+
+# TWFE estimates for full sample by assessed value
+rd_stag_cuts <- rd %>% 
+  group_by(id) %>% 
+  summarise(TotalAssesmentValue=mean(TotalAssesmentValue)) %>%
+  inner_join(rd_stag) %>%
+  mutate(assess_bin = cut(TotalAssesmentValue, breaks_val))
+
+res_twfe_full_sample_annual_gas = feols(log(gas) ~ i(treated_post,assess_bin, ref=FALSE) | id + consyear^assess_bin, rd_stag_cuts  %>% filter(years_to_treatment != -2000), cluster=~id + consyear, combine.quick = FALSE)
+
+# Predict energy consumption with and without retrofits
+pred_gas_save <- rd_stag_cuts %>%
+  mutate(consumption_with_retrofits = predict(res_twfe_full_sample_annual_gas, .)) %>%
+  mutate(treated_post = FALSE) %>%
+  mutate(consumption_without_retrofits = predict(res_twfe_full_sample_annual_gas, .)) %>%
+  mutate(diff = consumption_with_retrofits - consumption_without_retrofits) %>%
+  mutate(gas_savings_gj = gas * (- exp(diff) + 1)) %>%
+# Assume all savings gas at $5.98/GJ (average pre-retrofit price); multiply by 12 for annual
+  mutate(dollar_savings_gas = gas_savings_gj * 5.98 * 12)
+
+hd <- pred_gas_save %>% 
+  group_by(id) %>% 
+  # Only keep observations for 2018
+  filter(consyear == 2018) %>% 
+  group_by(assess_bin) %>% 
+  summarise(mean_gas_savings=mean(dollar_savings_gas, na.rm=T), 
+            sd_gas_savings = sd(dollar_savings_gas, na.rm=T),
+            count = n(),
+            avg_value = mean(TotalAssesmentValue)) %>%
+  mutate(se_gas_savings = sd_gas_savings / sqrt(count))
+
+ggplot(hd, 
+       aes(x=avg_value,
+           y=mean_gas_savings,
+           ymin = mean_gas_savings - 1.96*se_gas_savings,
+           ymax = mean_gas_savings + 1.96*se_gas_savings)) + 
+  geom_point() +
+  geom_line() +
+  geom_ribbon(alpha = 0.1, fill="blue") +
+  scale_x_continuous(name="Average assessed value", labels=scales::dollar_format()) +
+  scale_y_continuous(name="Average annual gas savings", labels=scales::dollar_format()) +
+  theme_bw() +
+  coord_cartesian(ylim=c(0,NA)) +
+  geom_hline(yintercept = 0)
+ggsave("../output_figures_tables/gas_save_vs_assessvalue_all.png", width=6, height=6)
+
+hd <- pred_gas_save %>% 
+  group_by(id) %>% 
+  # Only keep observations for 2018
+  filter(treated == TRUE) %>%
+  filter(consyear == 2018) %>% 
+  group_by(assess_bin) %>% 
+  summarise(mean_gas_savings=mean(dollar_savings_gas, na.rm=T), 
+            sd_gas_savings = sd(dollar_savings_gas, na.rm=T),
+            count = n(),
+            avg_value = mean(TotalAssesmentValue)) %>%
+  mutate(se_gas_savings = sd_gas_savings / sqrt(count))
+
+ggplot(hd, 
+       aes(x=avg_value,
+           y=mean_gas_savings,
+           ymin = mean_gas_savings - 1.96*se_gas_savings,
+           ymax = mean_gas_savings + 1.96*se_gas_savings)) + 
+  geom_point() +
+  geom_line() +
+  geom_ribbon(alpha = 0.1, fill="blue") +
+  scale_x_continuous(name="Average assessed value", labels=scales::dollar_format()) +
+  scale_y_continuous(name="Average annual gas savings", labels=scales::dollar_format()) +
+  theme_bw() +
+  coord_cartesian(ylim=c(0,NA)) +
+  geom_hline(yintercept = 0)
+ggsave("../output_figures_tables/gas_save_vs_assessvalue_participantsonly.png", width=6, height=6)
